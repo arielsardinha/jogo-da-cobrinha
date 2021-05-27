846535c379b763a30ab27f5e793dbb0b50b56156
Ariel Sardinha <odararepresentacoes@outlook.com.br>
2021-05-27 18:34:47 -0300

    cobrinha criada

--- js.js
+++ js.js
@@ -1,9 +1,18 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d'); // fala que o canvas vai rodar em 2d
 let box = 32;
+let snake = [];
+snake[0] = {x: 8 * box, y: 8 * box}; // tamanho da cobra
 
 function criarBG(){
     context.fillStyle = 'lightgreen'; // altera a cor do retangulo
-    context.fillRect(0 , 0, 16* box, 16 *box); //define os lados  x e y / largura e altura
+    context.fillRect(0 , 0, 16 * box, 16 * box); //define os lados  x e y / largura e altura
+}
+function criarCobrinha(){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = 'red';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 criarBG();
+criarCobrinha();
