6cc8eb8e573027c7d3145ca27d7e1aabc229cb63
Ariel Sardinha <odararepresentacoes@outlook.com.br>
2021-05-27 19:11:36 -0300

    fazendo a cobrinha andar e atravessar a tela

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ let snake = [];
 snake[0] = {x: 8 * box, y: 8 * box}; // tamanho da cobra
 let direcao = 'right'; 
 
-function criarBG(){
+function criarBG(){ // criando mapa
     context.fillStyle = 'lightgreen'; // altera a cor do retangulo
     context.fillRect(0 , 0, 16 * box, 16 * box); //define os lados  x e y / largura e altura
 }
@@ -16,7 +16,22 @@ function criarCobrinha(){ // criando cobrinha
     }
 }
 
-function iniciarJogo(){
+document.addEventListener('keydown', update); // faz evento de click
+function update(event){
+    // direcionando a cobra de acordo com a tecla digitada
+    if(event.keyCode == 37 && direcao != 'right') direcao = 'left';
+    if(event.keyCode == 38 && direcao != 'down') direcao = 'up';
+    if(event.keyCode == 39 && direcao != 'left') direcao = 'right';
+    if(event.keyCode == 40 && direcao != 'up') direcao = 'down';
+}
+
+function iniciarJogo(){ // iniciar jogo
+    // faz a cobra atravessar a parede
+    if(snake[0].x > 15 * box && direcao == 'right') snake[0].x = 0;
+    if(snake[0].x < 15 * box && direcao == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direcao == 'right') snake[0].y = 0;
+    if(snake[0].y < 15 * box && direcao == 'left') snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
     let snakeX = snake[0].x;
@@ -24,16 +39,14 @@ function iniciarJogo(){
 
     // cordenadas da cobrinha onde ela vai andar
     if(direcao == 'right') snakeX += box;
-    if(direcao == 'left') snakeY -= box;
+    if(direcao == 'left') snakeX -= box;
     if(direcao == 'up') snakeY -= box;
-    if(direcao == 'down') snakeX += box;
+    if(direcao == 'down') snakeY += box;
 
     // coloca a cobrinha pra andar sozinha
     snake.pop();
     let newHead = {x: snakeX, y: snakeY}
     snake.unshift(newHead);
-
-    
 }
 let jogo = setInterval(iniciarJogo, 100); // tempo para rodar o jogo 
 
